187de54e4ecb506e5f98f82af5be9ec5f148b9cb
verholantsev <verholantsev.filipp@yandex.ru>
2023-03-13 17:09:47 +0500

    initial commit

---
+++ src/main/java/com/intellecta/cinema/CinemaApplicationRun.java
@@ -0,0 +1,14 @@
+package com.intellecta.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApplicationRun {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CinemaApplicationRun.class, args);
+    }
+
+}
